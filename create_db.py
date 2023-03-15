436b756197296e9cb3af7ffc63c8a1ff7fad6348
matruby <Matthew.Rubie@flemingcollege.ca>
2023-03-15 14:28:43 -0400

    Finished everything in apod_desktop.py other than process statements

---
+++ create_db.py
@@ -0,0 +1,41 @@
+"""
+Description:
+ Creates the people table in the Social Network database
+ and populates it with 200 fake people.
+
+Usage:
+ python create_db.py
+"""
+import os
+import inspect
+import sqlite3
+
+def main():
+    global db_path
+    db_path = os.path.join(get_script_dir(), 'social_network.db')
+    create_people_table()
+    populate_people_table()
+
+def create_people_table():
+    """Creates the people table in the database"""
+    # TODO: Create function body
+    # Connect to the database 
+    people_db = sqlite3.connect()
+    return
+
+def populate_people_table():
+    """Populates the people table with 200 fake people"""
+    # TODO: Create function body
+    return
+
+def get_script_dir():
+    """Determines the path of the directory in which this script resides
+
+    Returns:
+        str: Full path of the directory in which this script resides
+    """
+    script_path = os.path.abspath(inspect.getframeinfo(inspect.currentframe()).filename)
+    return os.path.dirname(script_path)
+
+if __name__ == '__main__':
+   main()
